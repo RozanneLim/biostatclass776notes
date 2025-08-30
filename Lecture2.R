800e3fd78e2d902541f19f896538427563b50c36
rozanne lim <rozannelim@gmail.com>
2025-08-30 16:42:45 -0400

    Updated Lecture 2

--- Lecture2.R
+++ Lecture2.R
@@ -7,20 +7,20 @@ tools::package_dependencies("ggplot2")
 ##remotes::install_github()
 
 ##cloning repository from github
-#git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY.git
+git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY.git
 
 ## in terminal: repo new working directory, list the files, read the README file, show the remote URL
-#cd myrepo
-#ls
-#head README.md
-#git remote show origin
+cd myrepo
+ls
+head README.md
+git remote show origin
 
 ## in terminal: add a line to the README file and check the status of the repository
-#echo "A line I wrote on my local computer  " >> README.md
-#git status
+echo "A line I wrote on my local computer  " >> README.md
+git status
 
 ##rewriting credentials
-#gitcreds::gitcreds_set()
+gitcreds::gitcreds_set()
 
 ##code for creating a new R project and pushing it to github
 if (!requireNamespace("usethis", quietly = TRUE)) {
