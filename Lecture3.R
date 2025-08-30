aa142db7c2781abab93ad52cb24ecf0e1961960b
rozanne lim <rozannelim@gmail.com>
2025-08-30 16:36:53 -0400

    Updated Lecture 2 and 3

---
+++ Lecture3.R
@@ -0,0 +1,11 @@
+#Lecture 3: Git & Github [08/28/25]
+
+##installing usethis package
+if (!requireNamespace("usethis", quietly = TRUE)) {
+  install.packages("usethis")
+}
+
+##creating a folder using the usethis package
+usethis::create_project("~/Documents/biostat776classnotes")
+
+##making a public repository on Github
