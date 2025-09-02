79b102340d2c47ad6bf6fceca76678abbffe8e74
rozanne lim <rozannelim@gmail.com>
2025-09-02 16:04:20 -0400

    updated Lecture 3 - github w/ github code usethis

--- Lecture3.R
+++ Lecture3.R
@@ -9,3 +9,21 @@ if (!requireNamespace("usethis", quietly = TRUE)) {
 usethis::create_project("~/Documents/biostat776classnotes")
 
 ##making a public repository on Github
+
+#code for use this and github
+## Create an Rstudio project
+usethis::create_project("~/Desktop/biostat776project1")
+
+## Start version controlling it
+usethis::use_git()
+
+## Use the gh-pages branch in order for
+## GitHub pages https://pages.github.com/ to
+## host our website.
+usethis::git_default_branch_rename(to = "gh-pages")
+
+## Create a .nojekyll file
+writeLines("", here::here(".nojekyll"))
+
+## Share it via GitHub with the world
+usethis::use_github()
