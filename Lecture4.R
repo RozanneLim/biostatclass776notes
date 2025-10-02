253370ef99f5654dfb2534e940f4dfb0b30371c7
rozanne lim <rozannelim@gmail.com>
2025-10-02 16:57:16 -0400

    Updated previous lectures notes

--- Lecture4.R
+++
@@ -1,7 +0,0 @@
-#Lecture 4: Reproducible Research [08/28/2025]
-
-#session info is excellent for sharing all details about the R packages you are using for a particular script.
-install.packages(c("here", "sessioninfo"))
-sessioninfo::session_info()
-
-#here package helps with file paths, relative to your project home
